f7d49fbaa66bf1829848793b023948818f59c4b0
Thibault iTech <tallancon@gmail.com>
2015-10-04 12:28:30 +0200

    Removed tmp files

--- conversion.sh
+++ conversion.sh
@@ -2,6 +2,7 @@
 
 # Convert all .md files from pages/ into .html files and add header, title and footer
 find ./pages -iname "*.md" -type f -exec sh -c \
-   'pandoc --ascii "${0}" -o "./tmp/$(basename ${0%.md}.tmp.html)" && \
-   cat ./templates/layout/header.html ./tmp/$(basename ${0%.md}.tmp.html) ./templates/layout/footer.html > ./templates/pages/$(basename ${0%.md}.html) && \
-   sed -i "s/TITLE/$(basename ${0%.md}) - NapNac/g" ./templates/pages/$(basename ${0%.md}.html)' {} \;
+   'pandoc --ascii "${0}" -o "./$(basename ${0%.md}.tmp.html)" && \
+   cat ./templates/layout/header.html ./$(basename ${0%.md}.tmp.html) ./templates/layout/footer.html > ./templates/pages/$(basename ${0%.md}.html) && \
+   sed -i "s/TITLE/$(basename ${0%.md}) - NapNac/g" ./templates/pages/$(basename ${0%.md}.html) && \
+   rm ./$(basename ${0%.md}.tmp.html)' {} \;
