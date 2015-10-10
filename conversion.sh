695137dd921998aa987b27b579b6feac5fc00edf
Thibault <tallancon@gmail.com>
2015-10-10 11:09:25 +0200

    Added nginx config + Remade directory tree

--- conversion.sh
+++ conversion.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # First, get rid of all old .html files
-rm -r ./templates/pages/
-mkdir ./templates/pages/
+rm src/*.html
 
 # Then, convert all .md files from pages/ into .html files and add header, title and footer
-find ./pages -iname "*.md" -type f -exec sh -c \
-   'pandoc --ascii "${0}" -o "./$(basename ${0%.md}.tmp.html)" && \
-   cat ./templates/layout/header.html ./$(basename ${0%.md}.tmp.html) ./templates/layout/footer.html > ./templates/pages/$(basename ${0%.md}.html) && \
+find pages -iname "*.md" -type f -exec sh -c \
+   'pandoc --ascii "${0}" -o "$(basename ${0%.md}.tmp.html)" && \
+   cat src/layout/header.html $(basename ${0%.md}.tmp.html) src/layout/footer.html > src/$(basename ${0%.md}.html) && \
    title=`head -n 1 ${0}` && \
-   sed -i "s/TITLE/$title - NapNac/g" ./templates/pages/$(basename ${0%.md}.html) && \
-   rm ./$(basename ${0%.md}.tmp.html)' {} \;
+   sed -i "s/TITLE/$title - NapNac/g" src/$(basename ${0%.md}.html) && \
+   rm $(basename ${0%.md}.tmp.html)' {} \;
