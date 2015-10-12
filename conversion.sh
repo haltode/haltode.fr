6330394a23d26a7e5a3498679529afc089b4f148
Gnomino <Gnomino@users.noreply.github.com>
2015-10-12 20:04:18 +0200

    Optimized the files moving

--- conversion.sh
+++ conversion.sh
@@ -5,23 +5,15 @@ echo "<style>" > src/static/css/layout.css.html
 cat src/static/css/layout.css >> src/static/css/layout.css.html
 echo "</style>" >> src/static/css/layout.css.html
 
-# Pre-generate tag system to move converted files to their categories
-rm categories.sh
-touch categories.sh
-chmod +x categories.sh
-
 # Convert all .md files from pages/ into .html files and add header, title and footer
 find pages -iname "*.md" -type f -exec sh -c \
   'location=`sed -n "3p" ${0}` && \
-   echo "mv src/$(basename ${0%.md}.html) src/$location" >> categories.sh && \
    sed "3d" ${0} | pandoc --ascii -o "$(basename ${0%.md}.tmp.html)" && \
    cat src/layout/header.html $(basename ${0%.md}.tmp.html) src/layout/footer.html > src/$(basename ${0%.md}.html) && \
    title=`sed -n "1p" ${0}` && \
    sed -i "s/TITLE/$title - NapNac/g" src/$(basename ${0%.md}.html) && \
    sed -i "/CSS/r src/static/css/layout.css.html" src/$(basename ${0%.md}.html) && \
-   rm $(basename ${0%.md}.tmp.html)' {} \;
+   rm $(basename ${0%.md}.tmp.html) && \
+   mv -v src/$(basename ${0%.md}.html) src/$location' {} \;
 
-# Move all converted files to their categories
-./categories.sh
-
-   
+mv -v src/{projets,accueil,articles,a_propos}.html src/
