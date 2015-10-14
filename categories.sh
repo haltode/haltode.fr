b4e6d0d43134985b41ab8a7a93930843db69b989
Thibault <tallancon@gmail.com>
2015-10-14 18:12:21 +0200

    Changed font + Increased line spacing + Modified tables' layout + Added syntax highlighting

--- categories.sh
+++ categories.sh
@@ -1,8 +1,8 @@
-mv src/ioi.html src/projets/
-mv src/projets.html src/
-mv src/nandtotetris.html src/projets/
-mv src/rhyddid.html src/projets/
-mv src/ascii_funfair.html src/projets/
-mv src/accueil.html src/
 mv src/articles.html src/
+mv src/projets.html src/
+mv src/rhyddid.html src/projets/
+mv src/ioi.html src/projets/
+mv src/ascii_funfair.html src/projets/
+mv src/nandtotetris.html src/projets/
 mv src/a_propos.html src/
+mv src/accueil.html src/
