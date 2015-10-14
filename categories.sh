64782be3820c2cba9b993dba3fc107675b4a6b55
Thibault <tallancon@gmail.com>
2015-10-14 18:23:50 +0200

    Removed unnecessary file

--- categories.sh
+++
@@ -1,8 +0,0 @@
-mv src/articles.html src/
-mv src/projets.html src/
-mv src/rhyddid.html src/projets/
-mv src/ioi.html src/projets/
-mv src/ascii_funfair.html src/projets/
-mv src/nandtotetris.html src/projets/
-mv src/a_propos.html src/
-mv src/accueil.html src/
