0302e48a520fbe4cca339dd19242893cb2c300b0
Thibault iTech <tallancon@gmail.com>
2015-10-14 23:04:05 +0200

    Added comments to conversion.sh script

--- conversion.sh
+++ conversion.sh
@@ -5,7 +5,15 @@ echo "<style>" > src/static/css/layout.css.html
 cat src/static/css/layout.css >> src/static/css/layout.css.html
 echo "</style>" >> src/static/css/layout.css.html
 
-# Convert all .md files from pages/ into .html files and add header, title and footer
+# Convert all .md files from pages/ into .html files
+# 1. Get location for the future converted .html file
+# 2. Convert the .md file to .tmp.html file
+# 3. Create the .html, containing header + .tmp.html + footer
+# 4. Get the title
+# 5. Insert the title
+# 6. Insert inline css
+# 7. Remove .tmp.html file
+# 8. Move the .html to its actual location in src/
 find pages -iname "*.md" -type f -exec sh -c \
   'location=`sed -n "3p" ${0}` && \
    sed "3d" ${0} | pandoc --ascii -o "$(basename ${0%.md}.tmp.html)" && \
