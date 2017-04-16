ea35bdb979c7715b945fef7e6da3c49ef92125ee
Thibault <tallancon@gmail.com>
2017-04-16 19:00:40 +0200

    Minor changes

--- scripts/test_server.sh
+++ scripts/test_server.sh
@@ -2,11 +2,10 @@ read -n 1 -r -p "Testing server? [Y/n] "
 echo
 if [[ $REPLY == "Y" || $REPLY == "y" || $REPLY == "" ]]
 then
-   printf "Press any key to stop testing the server\n\n"
+   printf "Press any key to stop testing the server.\n\n"
    cd website
    python -m http.server 8000 &
    read -n 1 -s -p ""
    kill $(pgrep -f "python -m http.server 8000")
-
-   printf "\n"
+   echo
 fi
