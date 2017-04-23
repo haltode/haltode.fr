3a39b7e4a4a6b9148645ea02b238de61a06b5261
Thibault <tallancon@gmail.com>
2017-04-23 15:00:23 +0200

    Restructured and cleaned main build script (added 'Page' class) + Renamed pages dir to core_pages

--- scripts/build.py
+++ scripts/build.py
@@ -5,91 +5,115 @@ import os
 import sys
 
 from markdown.extensions.toc import TocExtension
-# Python-Markdown third party extension
 import figureAltCaption
 
 
-ARTICLE_DIR      = 'content/articles'
-PAGE_DIR         = 'content/pages'
-TEMPLATE_DIR     = 'content/templates'
-WEBSITE_DIR      = 'website'
+ARTICLE_DIR = 'content/articles'
+CORE_PAGES_DIR = 'content/core_pages'
+TEMPLATE_DIR = 'content/templates'
+WEBSITE_DIR = 'website'
 
 ARTICLE_TEMPLATE = 'article.html'
-PAGE_TEMPLATE    = 'default.html'
+CORE_PAGES_TEMPLATE = 'default.html'
 
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_DIR))
 # To get rid of useless empty lines in the html output
-jinja_env.trim_blocks   = True
+jinja_env.trim_blocks = True
 jinja_env.lstrip_blocks = True
 
 
-def read_markdown(path):
-    with open(path, 'r') as f:
-        content = f.read()
+class Page(object):
 
-    # Parse the content
-    extensions = {
-        'markdown.extensions.extra', 
-        'markdown.extensions.meta',
-        TocExtension(marker='', anchorlink=True, toc_depth=2),
-        'figureAltCaption'
-    }
-    md       = markdown.Markdown(extensions)
-    html     = md.convert(content)
-    metadata = md.Meta
-    toc      = md.toc
+    def __init__(self, file_path, template_path):
+        self.file_path = file_path
+        self.template_path = template_path
+        self.html = ''
+        self.metadata = {}
+        self.toc = ''
 
-    # The metadata values are lists but we want strings
-    for key, value in metadata.items():
-        metadata[key.lower()] = ''.join(value)
-    # Format the metadata
-    metadata['path'] = os.path.join(metadata['path'], os.path.basename(path))
-    metadata['path'] = metadata['path'].replace('.md', '.html')
+    def render(self):
+        self.parse_markdown()
 
-    # Test if the toc is empty
-    if 'li' not in toc:
-        toc = ''
+        output_params = {
+            'html_content': self.html,
+            'metadata': self.metadata,
+            'toc': self.toc
+        }
+        template = jinja_env.get_template(self.template_path)
+        output = template.render(output_params)
 
-    return html, metadata, toc
+        output_path = os.path.join(WEBSITE_DIR, self.metadata['path'])
+        with open(output_path, 'w+') as f:
+            f.write(output)
+
+    def parse_markdown(self):
+        with open(self.file_path, 'r') as f:
+            content = f.read()
+
+        self.convert_markdown(content)
+
+        self.format_metadata()
+        if self.is_toc_empty():
+            self.toc = ''
+
+    def convert_markdown(self, content):
+        extensions = {
+            'markdown.extensions.extra',
+            'markdown.extensions.meta',
+            TocExtension(marker='', anchorlink=True, toc_depth=2),
+            'figureAltCaption'
+        }
+        md = markdown.Markdown(extensions)
+
+        self.html = md.convert(content)
+        self.metadata = md.Meta
+        self.toc = md.toc
+
+    def format_metadata(self):
+        for key, value in self.metadata.items():
+            self.metadata[key.lower()] = ''.join(value)
+
+        location = self.metadata['path']
+        basename = os.path.basename(self.file_path)
+        self.metadata['path'] = os.path.join(location, basename).replace('.md', '.html')
+
+    def is_toc_empty(self):
+        if 'li' not in self.toc:
+            return True
+        else:
+            return False
 
 
-def render(file_path, template_path):
-    template            = jinja_env.get_template(template_path)
-    html, metadata, toc = read_markdown(file_path)
-
-    output_params = {'html_content': html, 'metadata': metadata, 'toc': toc}
-    output_path   = os.path.join(WEBSITE_DIR, metadata['path'])
-    output        = template.render(output_params)
-    with open(output_path, 'w+') as f:
-        f.write(output)
+def get_files_path(directory, extension):
+    files_path = glob.iglob(directory + '/**/*.' + extension, recursive=True)
+    return list(files_path)
 
 
-def get_path_files(directory, extension):
-    return glob.iglob(directory + '/**/*.' + extension, recursive=True)
+def get_all_files_path():
+    files_path = get_files_path(CORE_PAGES_DIR, 'md')
+    files_path += get_files_path(ARTICLE_DIR, 'md')
+    return files_path
 
 
-def render_pages():
-    for page in get_path_files(PAGE_DIR, 'md'):
-        render(page, PAGE_TEMPLATE)
-
-
-def render_articles():
-    for article in get_path_files(ARTICLE_DIR, 'md'):
-        render(article, ARTICLE_TEMPLATE)
-
-
-# We can specify the files we only want to render
 if len(sys.argv) > 1:
-    for f in sys.argv[1:]:
-        print("Rendering: ", os.path.basename(f))
-        if PAGE_DIR in f:
-            render(f, PAGE_TEMPLATE)
-        elif ARTICLE_DIR in f:
-            render(f, ARTICLE_TEMPLATE)
-# Or, we can render all the files
+    files_to_render = sys.argv[1:]
 else:
-    print("Rendering pages...")
-    render_pages()
-    print("Rendering articles...")
-    render_articles()
-    print("Done!")
+    files_to_render = get_all_files_path()
+
+print("Rendering pages...")
+
+nb_pages_rendered = 0
+for file_path in files_to_render:
+    if CORE_PAGES_DIR in file_path:
+        template_path = CORE_PAGES_TEMPLATE
+    elif ARTICLE_DIR in file_path:
+        template_path = ARTICLE_TEMPLATE
+    else:
+        print("Error: unknown file path:", file_path)
+        continue
+
+    page = Page(file_path, template_path)
+    page.render()
+    nb_pages_rendered = nb_pages_rendered + 1
+
+print("Done:", nb_pages_rendered, "pages rendered.")
