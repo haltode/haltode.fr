cb92a6dcb6b6fc1d56719103475705339ab65372
Thibault <tallancon@gmail.com>
2017-04-29 20:12:57 +0200

    Fixed rsync command

--- scripts/update_server.sh
+++ scripts/update_server.sh
@@ -2,6 +2,6 @@ read -n 1 -r -p "Updating server? [Y/n] "
 echo
 if [[ $REPLY == "Y" || $REPLY == "y" || $REPLY == "" ]]
 then
-   rsync -ru --delete website/ pi@192.168.1.28:/home/pi/napnac/napnac.fr/
+   rsync -ru --delete website/* pi@192.168.1.28:/home/pi/napnac/napnac.fr
    printf "Done!\n"
 fi
