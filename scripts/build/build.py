2786a9405fde786f184cde41e9cc8c92dcfa8166
Thibault <tallancon@gmail.com>
2017-05-01 18:18:31 +0200

    Cleaned import modules

--- scripts/build/build.py
+++ scripts/build/build.py
@@ -1,25 +1,25 @@
 import sys
 
-from pages import Page
-from path import get_all_files_path, get_template_path
+import pages
+import path
 
 
 if len(sys.argv) > 1:
     files_to_render = sys.argv[1:]
 else:
-    files_to_render = get_all_files_path()
+    files_to_render = path.get_all_files()
 
 print("Rendering pages...")
 
 nb_pages_rendered = 0
 for file_path in files_to_render:
     try:
-        template_path = get_template_path(file_path)
+        template_path = path.get_template(file_path)
     except ValueError:
         print("Error: Unrecognized file path:", file_path)
         continue
 
-    page = Page(file_path, template_path)
+    page = pages.Page(file_path, template_path)
     page.render()
     nb_pages_rendered = nb_pages_rendered + 1
 
--- scripts/build/path.py
+++ scripts/build/path.py
@@ -8,18 +8,18 @@ ARTICLE_TEMPLATE = 'article.html'
 CORE_PAGES_TEMPLATE = 'default.html'
 
 
-def get_files_path(directory, extension):
+def get_files(directory, extension):
     files_path = glob.iglob(directory + '/**/*.' + extension, recursive=True)
     return list(files_path)
 
 
-def get_all_files_path():
-    files_path = get_files_path(CORE_PAGES_DIR, 'md')
-    files_path += get_files_path(ARTICLE_DIR, 'md')
+def get_all_files():
+    files_path = get_files(CORE_PAGES_DIR, 'md')
+    files_path += get_files(ARTICLE_DIR, 'md')
     return files_path
 
 
-def get_template_path(file_path):
+def get_template(file_path):
     if CORE_PAGES_DIR in file_path:
         return CORE_PAGES_TEMPLATE
     elif ARTICLE_DIR in file_path:
