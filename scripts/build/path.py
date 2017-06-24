3aebfa340cdb23d81b444e5ea50a09dd5f780950
Thibault <tallancon@gmail.com>
2017-06-24 15:07:34 +0200

    Improved naming

--- scripts/build/path.py
+++ scripts/build/path.py
@@ -2,10 +2,10 @@ import glob
 
 
 ARTICLE_DIR = 'content/articles'
-CORE_PAGES_DIR = 'content/core_pages'
+MAIN_PAGES_DIR = 'content/main_pages'
 
 ARTICLE_TEMPLATE = 'article.html'
-CORE_PAGES_TEMPLATE = 'default.html'
+MAIN_PAGES_TEMPLATE = 'base.html'
 
 
 def get_files(directory, extension):
@@ -14,14 +14,14 @@ def get_files(directory, extension):
 
 
 def get_all_files():
-    files_path = get_files(CORE_PAGES_DIR, 'md')
+    files_path = get_files(MAIN_PAGES_DIR, 'md')
     files_path += get_files(ARTICLE_DIR, 'md')
     return files_path
 
 
 def get_template(file_path):
-    if CORE_PAGES_DIR in file_path:
-        return CORE_PAGES_TEMPLATE
+    if MAIN_PAGES_DIR in file_path:
+        return MAIN_PAGES_TEMPLATE
     elif ARTICLE_DIR in file_path:
         return ARTICLE_TEMPLATE
     else:
