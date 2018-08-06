2491064f40113f71463d6f04d47da67fbe01f3b9
Thibault Allançon <tallancon@gmail.com>
2018-08-06 22:54:43 +0200

    add css and js folders

---
+++ js/utils.js
@@ -0,0 +1,13 @@
+function toggle_visibility(id) {
+    var element = document.getElementById(id);
+    if(element.style.display == 'block')
+        element.style.display = 'none';
+    else
+        element.style.display = 'block';
+
+    var icon = document.getElementById('toggle_arrow_' + id)
+    if(!icon.className.localeCompare('fa fa-angle-down'))
+        icon.className = 'fa fa-angle-up'
+    else
+        icon.className = 'fa fa-angle-down'
+}
