1977009f99f7a0ca267670a497621dbde05c503d
Thibault Allançon <haltode@gmail.com>
2021-11-17 19:27:08 +0100

    movies: tmdb2yml: can now specify movie name as input

--- content/misc/movies/tmdb2yml.py
+++ content/misc/movies/tmdb2yml.py
@@ -8,13 +8,14 @@ You will need a .secrets.toml file with the following content:
     api_key = "FIXME"
     api_url = "https://api.themoviedb.org/3"
 
-Then, provide movie TMDB ids (one per line). For example:
+Then, provide a list of movies (one per line, each line containing either a TMDB
+id or the name of the movie). For example:
 
     $ cat my_movies.txt
     49797
     22538
-    77
-    961
+    memento
+    the general
     $ cat my_movies.txt | ./tmdb2yml.py
     - title: I Saw the Devil
       year: 2010
@@ -33,11 +34,16 @@ Then, provide movie TMDB ids (one per line). For example:
       poster_url: https://image.tmdb.org/t/p/w154/dd2cviAare1rUoMiDmG2NuvFQEE.jpg
       tmdb_url: https://www.themoviedb.org/movie/961
 
+Note: using the name of the movie as input means defaulting to the first search
+result of TMDB website.
+
 """
 
 import json
 import logging
+import re
 import sys
+import urllib
 from typing import Any, Dict
 
 import dateutil.parser
@@ -61,10 +67,13 @@ class TMDBService:
         self.session = requests.Session()
         self.config = self.request("/configuration")
 
-    def request(self, endpoint: str) -> JSONType:
+    def request(self, endpoint: str, query: str=None) -> JSONType:
         if not endpoint.startswith("/"):
             raise ValueError("TMDB endpoint string must start with a '/'")
         url = f"{self.api_url}{endpoint}?api_key={self.api_key}"
+        if query:
+            url = f"{url}&{query}"
+
         try:
             r = self.session.get(url)
             return r.json()
@@ -83,16 +92,29 @@ class TMDBService:
 
 tmdb_service = TMDBService(".secrets.toml")
 metadatas = []
-for tmdb_id in sys.stdin:
+for query in sys.stdin:
     # Remove unwanted '\n' introduced by sys.stdin
-    tmdb_id = tmdb_id.rstrip()
-    movie = tmdb_service.request(f"/movie/{tmdb_id}")
-    metadatas.append(
-        {
-            "title": movie["title"],
-            "year": dateutil.parser.parse(movie["release_date"]).year,
-            "poster_url": tmdb_service.get_poster_url(movie["poster_path"]),
-            "tmdb_url": f"https://www.themoviedb.org/movie/{tmdb_id}",
-        }
-    )
+    query = query.rstrip()
+    if re.match(r"[0-9]+", query):
+        tmdb_id = query
+    else:
+        escaped_query = f"query={urllib.parse.quote(query)}"
+        search = tmdb_service.request(f"/search/movie", escaped_query)
+        if search["results"]:
+            tmdb_id = search["results"][0]["id"]
+        else:
+            tmdb_id = None
+
+    try:
+        movie = tmdb_service.request(f"/movie/{tmdb_id}")
+        metadatas.append(
+            {
+                "title": movie["title"],
+                "year": dateutil.parser.parse(movie["release_date"]).year,
+                "poster_url": tmdb_service.get_poster_url(movie["poster_path"]),
+                "tmdb_url": f"https://www.themoviedb.org/movie/{tmdb_id}",
+            }
+        )
+    except Exception as err:
+        logging.error(f"error while processing {query}: err={err}, metadata={movie}")
 print(yaml.dump(metadatas, sort_keys=False))
